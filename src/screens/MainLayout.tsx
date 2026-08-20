2dc536078ebf914101d8b6e84313fe07831b6841
goutamyadav-yadav <goutam.y@intefai.com>
2026-08-20 15:27:38 +0530

    support screen work

--- src/screens/MainLayout.tsx
+++ src/screens/MainLayout.tsx
@@ -379,6 +379,7 @@ export default function MainLayout() {
           <SupportScreen
             onBack={closeOverlay}
             onNotifications={openNotifications}
+            isMitra={isMitra}
           />
         );
       case 'news':
